b2025b33d7feb7700bd905bab09c26de7e859f19
CordlessCoder <>
2022-12-30 20:17:53 +0000

    truncate URLs

--- src/handlers_shorten.rs
+++ src/handlers_shorten.rs
@@ -4,7 +4,7 @@ use crate::{
     state::{CurState, Entry},
     Redirect, StatusCode, Url, UrlPath, IP, URL_CF, URL_ID_LENGTH,
 };
-use axum::{extract::State, http::HeaderMap,response::IntoResponse};
+use axum::{extract::State, http::HeaderMap, response::IntoResponse};
 use lazy_static::lazy_static;
 
 pub async fn get_url(
@@ -45,9 +45,10 @@ pub async fn delete_url(
 
 pub async fn create_url(
     UrlPath(short): UrlPath<String>,
-    url: String,
+    mut url: String,
     State(state): State<CurState>,
 ) -> Result<(StatusCode, String), (StatusCode, &'static str)> {
+    url.truncate(2048);
     let length = short.len();
     if length > 16 || length <= 1 {
         return Err((StatusCode::BAD_REQUEST, "custom ID length out of bounds"));
@@ -90,10 +91,8 @@ pub async fn create_url(
     }
 }
 
-pub async fn shorten_url(
-    State(state): State<CurState>,
-    url: String,
-) -> impl IntoResponse {
+pub async fn shorten_url(State(state): State<CurState>, mut url: String) -> impl IntoResponse {
+    url.truncate(2048);
     let Ok(parsed_url) = Url::parse(&url) else {
         return (
             StatusCode::UNPROCESSABLE_ENTITY,
@@ -109,7 +108,8 @@ pub async fn shorten_url(
         return (
             StatusCode::UNSUPPORTED_MEDIA_TYPE,
             "Cannot shorten this URL",
-        ).into_response();
+        )
+            .into_response();
     }
 
     let id = id::Id::new(URL_ID_LENGTH).into_inner();
@@ -126,7 +126,8 @@ pub async fn shorten_url(
             std::str::from_utf8_unchecked(&id) // unsafe used here as the id has to be correct UTF-8 as
                                                // we just generated it
         }),
-    ).into_response()
+    )
+        .into_response()
 }
 
 lazy_static! {
