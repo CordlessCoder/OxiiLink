46651818100db07848e0c4f9676cd037b6404793
CordlessCoder <>
2023-02-03 12:26:54 +0000

    added graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use rocksdb::{self, DB};
 use std::net::SocketAddr;
 use std::sync::Arc;
 use stretto::AsyncCache;
+use tokio::signal;
+use tokio::signal::unix::SignalKind;
 use url::Url;
 
 mod bot;
@@ -109,9 +111,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // axum_server::bind_rustls(addr, config)
     axum::Server::bind(&addr)
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+        .with_graceful_shutdown(shutdown_signal())
         .await?;
     Ok(())
 }
 pub async fn status() -> &'static str {
     "Ok!"
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("termination signal received, starting graceful shutdown");
 }
